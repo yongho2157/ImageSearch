adae17cc2a140656704df0cf5d3f70a06571202c
yonghoChoi2157 <youngho2157@naver.com>
2022-06-15 23:45:21 +0900

    [Add] detailActivity

---
+++ app/src/main/java/com/yh/imagesearch/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yh.imagesearch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yh.imagesearch.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
